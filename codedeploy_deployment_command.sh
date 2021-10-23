2843f88095ca6e4f4028ce45fb2369307f3d04fe
Brandon Rich <brich@nd.edu>
2021-10-23 10:11:22 -0400

    codedeploy still balked at overwriting nginx.conf so we will delete it

--- codedeploy_deployment_command.sh
+++ codedeploy_deployment_command.sh
@@ -1,6 +1,7 @@
-aws deploy create-deployment \ 
+aws deploy create-deployment \
  --application-name blog \
  --deployment-config-name CodeDeployDefault.OneAtATime \
  --deployment-group-name blog_deployment_group \
  --description "Demo Deployment" \
- --github-location repository=brandonrich/ruby-hello,commitId=$(git rev-parse HEAD)
+ --github-location repository=brandonrich/ruby-hello,commitId=$(git rev-parse HEAD) \
+ --profile linkedin-admin 
