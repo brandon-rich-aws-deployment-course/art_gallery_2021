0722d0ab9dea83c2ac52de67c860daa33d417903
Brandon Rich <brich@nd.edu>
2021-10-23 10:14:32 -0400

    run rails through bundler

--- codedeploy/scripts/start_rails_server.sh
+++ codedeploy/scripts/start_rails_server.sh
@@ -6,4 +6,4 @@ bundle config build.nokogiri --use-system-libraries
 bundle
 bundle exec rake db:migrate
 #bundle exec unicorn -E development -c /opt/current-deployment/config/unicorn.rb -D
-rails s -d
+bundle exec rails s -d
