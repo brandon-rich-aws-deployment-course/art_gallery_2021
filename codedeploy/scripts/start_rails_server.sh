91341ff603663b3c7f49ec73ebe1e81b433be305
Brandon Rich <brich@nd.edu>
2021-10-24 09:50:36 -0400

    try putting ENV in the same line as the server start

--- codedeploy/scripts/start_rails_server.sh
+++ codedeploy/scripts/start_rails_server.sh
@@ -6,4 +6,9 @@ bundle config build.nokogiri --use-system-libraries
 bundle
 bundle exec rake db:migrate
 #bundle exec unicorn -E development -c /opt/current-deployment/config/unicorn.rb -D
-bundle exec rails s -d
+bundle exec rails s -d \
+ RDS_PORT=5432 \
+ RDS_DB_NAME="postgres" \
+ RDS_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresPassword --with-decryption --query Parameters[0].Value) \
+ RDS_USERNAME=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresUsername --with-decryption --query Parameters[0].Value) \
+ RDS_ENDPOINT=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresEndpoint --with-decryption --query Parameters[0].Value)
