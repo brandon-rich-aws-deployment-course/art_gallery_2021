e165c3bedd7785cbabc43b812c24cc3de7eefae1
Brandon Rich <brich@nd.edu>
2021-10-24 09:54:13 -0400

    try ENV a different way, in startup script

--- codedeploy/scripts/start_rails_server.sh
+++ codedeploy/scripts/start_rails_server.sh
@@ -2,13 +2,13 @@
 . /home/ec2-user/.bashrc
 export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin
 cd /opt/current-deployment
+export RDS_PORT=5432
+export RDS_DB_NAME="postgres"
+export RDS_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresPassword --with-decryption --query Parameters[0].Value)
+export RDS_USERNAME=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresUsername --with-decryption --query Parameters[0].Value)
+export RDS_ENDPOINT=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresEndpoint --with-decryption --query Parameters[0].Value)
 bundle config build.nokogiri --use-system-libraries
 bundle
 bundle exec rake db:migrate
 #bundle exec unicorn -E development -c /opt/current-deployment/config/unicorn.rb -D
-bundle exec rails s -d \
- RDS_PORT=5432 \
- RDS_DB_NAME="postgres" \
- RDS_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresPassword --with-decryption --query Parameters[0].Value) \
- RDS_USERNAME=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresUsername --with-decryption --query Parameters[0].Value) \
- RDS_ENDPOINT=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresEndpoint --with-decryption --query Parameters[0].Value)
+bundle exec rails s -d
