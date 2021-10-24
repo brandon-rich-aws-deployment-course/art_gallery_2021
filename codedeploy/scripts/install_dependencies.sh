29de1c4cf1f92a91f4643963892a3c4cc5a78485
Brandon Rich <brich@nd.edu>
2021-10-24 14:54:11 -0400

    put user data setup in appspec instead

--- codedeploy/scripts/install_dependencies.sh
+++ codedeploy/scripts/install_dependencies.sh
@@ -2,3 +2,19 @@ sudo amazon-linux-extras install ruby3.0 -y
 sudo yum install -y sqlite-devel ruby-devel gcc-c++ gcc libxml2 libxml2-devel libxslt libxslt-devel postgresql-devel
 #sudo gem install io-console
 sudo gem install bundler
+
+
+sudo yum update -y
+
+# install nvm
+cd ~
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install 14.18.1
+
+#install yarn
+npm install yarn -g
