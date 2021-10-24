f83b004d4843adf77f4d7f660ec047e2eca3705b
Brandon Rich <brich@nd.edu>
2021-10-24 19:04:54 -0400

    script cleanup

--- codedeploy/scripts/start_rails_server.sh
+++ codedeploy/scripts/start_rails_server.sh
@@ -1,9 +1,9 @@
-. /home/ec2-user/.bash_profile
-. /home/ec2-user/.bashrc
-export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin
-
+#. /home/ec2-user/.bash_profile
+#. /home/ec2-user/.bashrc
+#export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin
 
 # install nvm
+echo "DEBUG: Install Node via NVM and install yarn"
 cd ~
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
@@ -17,43 +17,40 @@ nvm install 14.18.1
 npm install yarn -g
 
 # symlink it so it's in the PATH
-echo "BMR DEBUG YARN"
+echo "DEBUG: Get yarn in PATH"
 ln -s -f "$(npm bin --global)"/yarn /usr/bin/yarn
-echo "BMR WHICH YARN"
-echo $(which yarn)
-echo "BMR DONE"
 
 
-
-
+# move to current deployment folder.  Note: this is a symlink to
+# the latest deploy in /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 cd /opt/current-deployment
 
 
-
-echo "BMR DEBUG POINT"
+# NOTE: parameter store retrievals will work as long as the instance
+# has attached an IAM role whose policies include get privileges on these
+# keys (such as CodeDeployPostgresPassword)
+echo "DEBUG: Adding ENV variables from Parameter Store"
 export RDS_PORT=5432
 export RDS_DB_NAME="postgres"
 export RDS_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresPassword --with-decryption --query Parameters[0].Value)
 export RDS_USERNAME=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresUsername --with-decryption --query Parameters[0].Value)
 export RDS_HOSTNAME=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresEndpoint --with-decryption --query Parameters[0].Value)
-echo "BMR PRINT ENV"
-env
-echo "BMR DONE PRINT ENV"
 
-echo "BMR WHICH YARN2"
-echo $(which yarn)
-echo "BMR DONE"
-
+echo "DEBUG: Generating a secret key base value"
 export SECRET_KEY_BASE=$(bundle exec rake secret)
 
+echo "DEBUG: Installing gem dependencies"
 bundle config build.nokogiri --use-system-libraries
 bundle
 
 # need node and yarn
+echo "DEBUG: Precompiling assets"
 rails assets:precompile
 
+echo "DEBUG: Performing database migration, if needed"
 bundle exec rake db:migrate
-#bundle exec unicorn -E development -c /opt/current-deployment/config/unicorn.rb -D
+
+echo "DEBUG: Starting the server (puma) in daemon mode.  Pid will appear in ./tmp/pids/server.pid"
 bundle exec rails s -d
 
 
