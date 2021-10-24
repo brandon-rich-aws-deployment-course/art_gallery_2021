7cde1aa8d346430298868b65872ce812f5f189fb
Brandon Rich <brich@nd.edu>
2021-10-24 10:13:56 -0400

    debug appspec

--- codedeploy/scripts/start_rails_server.sh
+++ codedeploy/scripts/start_rails_server.sh
@@ -8,6 +8,9 @@ export RDS_DB_NAME="postgres"
 export RDS_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresPassword --with-decryption --query Parameters[0].Value)
 export RDS_USERNAME=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresUsername --with-decryption --query Parameters[0].Value)
 export RDS_ENDPOINT=$(aws ssm get-parameters --region us-east-1 --names CodeDeployPostgresEndpoint --with-decryption --query Parameters[0].Value)
+echo "BMR PRINT ENV"
+env
+echo "BMR DONE PRINT ENV"
 bundle config build.nokogiri --use-system-libraries
 bundle
 bundle exec rake db:migrate
