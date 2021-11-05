f28be4ef5cf6abfa400cb0ed6915981a05ee7f92
Brandon Rich <brich@nd.edu>
2021-11-04 23:15:24 -0400

    nicer styling

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -28,7 +28,7 @@ class ArtsController < ApplicationController
     respond_to do |format|
       if @art.save
         format.html { redirect_to @art, notice: "Art was successfully created." }
-        format.json { render :show, status: :created, location: @art }
+        format.json { render :index, status: :created, location: @art }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @art.errors, status: :unprocessable_entity }
