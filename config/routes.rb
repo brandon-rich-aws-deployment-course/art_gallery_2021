3ba5f6b18df825e1b98fda8df745601b1a532532
Brandon Rich <brich@nd.edu>
2021-11-14 20:06:15 -0500

    removed old code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :arts
-  resources :albums
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "arts#index"
   get "/articles", to: "articles#index"
