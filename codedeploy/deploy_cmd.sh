2afc6f8af5774c838153a26896b90a7178b7d63e
Recording Booth <booth@brandons-mbp.dhcp.nd.edu>
2021-12-13 15:33:55 -0500

    added comment

--- codedeploy/deploy_cmd.sh
+++ codedeploy/deploy_cmd.sh
@@ -1,3 +1,5 @@
+# If you have forked your own copy of the repo, replace repo account 
+# brandon-rich-aws-deployment-course with your own account name.
 aws deploy create-deployment \
  --application-name art_gallery \
  --deployment-config-name CodeDeployDefault.OneAtATime \
