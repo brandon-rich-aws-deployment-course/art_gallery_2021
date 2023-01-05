fa0774507a67b5feae4028e8082d502991e1fad6
albertgong <84435089+albertgongsun@users.noreply.github.com>
2023-01-05 16:53:20 +0800

    rollback to ruby 3.0

--- codedeploy/scripts/install_dependencies.sh
+++ codedeploy/scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
-sudo amazon-linux-extras install ruby2.6 -y
+sudo amazon-linux-extras install ruby3.0 -y
 sudo yum install -y sqlite-devel ruby-devel gcc-c++ gcc libxml2 libxml2-devel libxslt libxslt-devel postgresql-devel
 sudo gem install bundler
 sudo yum update -y
